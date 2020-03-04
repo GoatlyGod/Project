ec55d76141b255d0f3e2f5a177b851c0cc1480a7
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-04 14:13:51 -0500

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import "../src/App.css";
-import InfoCard from './components/InfoCard';
-import Navbar from './components/Navbar'
+import Home from "./views/home"
 
 
 const App = () => {
   return (
-    <React.Fragment>
-     <Navbar />
-      <div className="Card box2 container">
-          <div className="row">
-            <div className="box1 d-flex flex-col flex-nowrap">
-              <InfoCard />
-              <InfoCard />
-              <InfoCard />
-              <InfoCard />
-              <InfoCard />
-            </div>
-          </div>
-      </div>
+      <React.Fragment>
+        <Home/>
       </React.Fragment>
   );
 }
---
+++ src/views/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+
+const home = () => {
+    return(
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    )
+}
+
+export default home;
