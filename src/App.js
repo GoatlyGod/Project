0bd32114002374f330e7c0ed47f9dabce801b8bb
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-04 11:47:13 -0500

    Files Made

--- src/App.js
+++ src/App.js
@@ -7,56 +7,7 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <React.Fragment>
-    <div>
-      <nav className="navbar navbar-dark bg-dark fixed-top">
-        <form className="form-inline">
-          <button
-            type="button active"
-            className="btn btn-outline-dark text-light"
-          >
-            <i className="fa fa-facebook"></i>
-          </button>
-          <button
-            type="button active"
-            className="btn btn-outline-dark text-secondary"
-          >
-            <i className="fa fa-twitter"></i>
-          </button>
-          <button
-            type="button active"
-            className="btn btn-outline-dark text-secondary"
-          >
-            <i className="fa fa-instagram"></i>
-          </button>
-          <button
-            type="button active"
-            className="btn btn-outline-dark text-secondary"
-          >
-            <i className="fa fa-youtube"></i>
-          </button>
-        </form>
-
-        <b className="navbar-brand" href="#">
-          STARWARS
-        </b>
-
-        <div class="d-flex">
-          <div class="dropdown mr-1">
-            <button
-              type="button"
-              class="btn btn-secondary dropdown-toggle"
-              id="dropdownMenuOffset"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-              data-offset="10,20"
-            >
-              Favorites
-            </button>
-          </div>
-        </div>
-      </nav>
-    </div>   
+     <Navbar />
       <div className="Card box2 container">
           <div className="row">
             <div className="box1 d-flex flex-col flex-nowrap">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,59 +3,31 @@ import React from 'react'
 
 
 const Navbar = () => {
-    return (
-        <div>
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-          <form className="form-inline">
-            <button
-              type="button active"
-              className="btn btn-outline-dark text-light"
-            >
-              <i className="fa fa-facebook"></i>
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <button
-              type="button active"
-              className="btn btn-outline-dark text-secondary"
-            >
-              <i className="fa fa-twitter"></i>
-            </button>
-            <button
-              type="button active"
-              className="btn btn-outline-dark text-secondary"
-            >
-              <i className="fa fa-instagram"></i>
-            </button>
-            <button
-              type="button active"
-              className="btn btn-outline-dark text-secondary"
-            >
-              <i className="fa fa-youtube"></i>
-            </button>
-          </form>
+            <a className="navbar-brand" href="#">Navbar</a>
 
-          <b className="navbar-brand" href="#">
-            STARWARS
-          </b>
-
-          <div class="d-flex">
-            <div class="dropdown mr-1">
-              <button
-                type="button"
-                class="btn btn-secondary dropdown-toggle"
-                id="dropdownMenuOffset"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                data-offset="10,20"
-              >
-                Favorites
-              </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
             </div>
-          </div>
         </nav>
-      </div>   
     )
 }
-
-
 export default Navbar;
