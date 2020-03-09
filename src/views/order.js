a2f75ff17cf8ed98125c6f72ed8ba4a834908ef0
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-09 10:16:10 -0400

    ehh

--- src/views/order.js
+++ src/views/order.js
@@ -1,11 +1,42 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import "./Order.css"
+import "./Order.css";
+
 const Order = () => {
   return (
     <div>
       <Navbar />
-      <h1>test</h1>
+
+      <div>
+
+      <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+      </div>
     </div>
   );
 };
