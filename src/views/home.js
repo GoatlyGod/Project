cf6a3b03c4d759263520dd3e491ca9060d2143c7
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-10 14:16:00 -0400

    More JS

--- src/views/home.js
+++ src/views/home.js
@@ -18,12 +18,18 @@ const Home = () => {
             <div className="carousel-inner">
                 <div className="carousel-item active">
                     <img src={STX} className="d-block w-100" alt="..."/>
+                    <div className="carousel-caption d-none d-md-block">
+                        <h1>St. Croix</h1>
+                    </div>
                 </div>
                 <div className="carousel-item">
                     <img src={STT} className="d-block w-100" alt="..."/>
                 </div>
                 <div className="carousel-item">
                     <img src={STJ} className="d-block w-100" alt="..."/>
+                    <div className="carousel-caption d-none d-md-block">
+                        <h1>St. John</h1>
+                    </div>
                 </div>
             </div>
             <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
--- src/views/login.js
+++ src/views/login.js
@@ -1,67 +1,89 @@
 import React, { useState } from "react";
 
 import "./Login.css";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
+  const history = useHistory();
+
+  const formOnSubmitHandler = event => {
+    event.preventDefault();
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: inputEmail,
+        password: inputPassword
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.token) {
+          history.push("/home");
+        } else if (data.error) {
+          alert(data.error);
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div>
       <Navbar />
+      <form onSubmit={formOnSubmitHandler}>
+        <div className="loginme">
+          <h3>Please Login into your account</h3>
 
-      <div className="loginme">
-        <h3>Please Login into your account</h3>
+          <div className="form-group">
+            Email
+            <input
+              type="text"
+              className="form-control"
+              placeholder="underrated@email.com"
+              value={inputEmail}
+              name={inputEmail}
+              onChange={e => setInputEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            Password
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Your Password *"
+              value={inputPassword}
+              name={inputPassword}
+              onChange={e => setInputPassword(e.target.value)}
+            ></input>
+          </div>
 
-        <div className="form-group">
-          Email
-          <input
-            type="text"
-            className="form-control"
-            placeholder="underrated@email.com"
-            value={inputEmail}
-            name={inputEmail}
-            onChange={e => setInputEmail(e.target.value)}
-          />
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary btnSubmit"
+              value="Login"
+            >
+              Login
+            </button>
+          </div>
+
+          <div className="form-group">
+            <a href="..." className="btnForgetPwd">
+              Forget Password?
+            </a>
+          </div>
         </div>
-        <div className="form-group">
-          Password
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Your Password *"
-            value={inputPassword}
-            name={inputPassword}
-            onChange={e => setInputPassword(e.target.value)}
-          ></input>
-        </div>
+      </form>
 
-          
-            <div className="form-group">
-              <button
-                type="submit"
-                className="btn btn-primary btnSubmit"
-                value="Login"
-              >
-                <Link to="/home">
-                  Login
-                </Link>
-                
-              </button>
-            </div>
-         
-        
-
-        <div className="form-group">
-          <a href="..." className="btnForgetPwd">
-            Forget Password?
-          </a>
-        </div>
-      </div>
       <Footer />
-
     </div>
   );
 };
