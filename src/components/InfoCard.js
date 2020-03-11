552659ddef08ff336d1a482d58569b639df9712f
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-11 15:22:56 -0400

    Partying Now

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,159 +1,91 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
+const InfoCard = props => {
+  return (
+    <React.Fragment>
+            <div className="card mr-5 mt-5">
+              <img
+                src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg"
+                className="card-img-top"
+                alt="..."
+                style={{ width: "90%", margin: "15px", borderRadius: "15px" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  <b>{props.name}</b>
+                </h5>
+                <h6 className="price">{props.price}</h6>
+                <p className="card-text">
+                  3 sweet boiled dumplings and saltfish with island touch
+                  seasonings
+                </p>
 
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  data-toggle="modal"
+                  data-target="#exampleModalScrollable"
+                >
+                  Add to cart
+                </button>
 
-const InfoCard = () => {
-    return (
-        <React.Fragment> 
-          <div className="container mt-3">
-            <div className="row">
-              <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg" className="card-img-top" alt="..." style={{width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title"><b>Saltfish&Dumplings</b></h5>
-                    <h6 className="price" >$11.35</h6>
-                    <p className="card-text">3 sweet boiled dumplings and saltfish with island touch seasonings</p>
-                    
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
-                      Add to cart
-                    </button>
-
-                    <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-                      <div class="modal-dialog modal-dialog-scrollable" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalScrollableTitle">Saltfish&Dumplings</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                          <img src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg" className="card-img-top" alt="..." style={{width: "90%", margin: "15px",  borderRadius: "15px" }} />
-
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Add to cart- $11.35</button>
-                          </div>
-                        </div>
+                <div
+                  class="modal fade"
+                  id="exampleModalScrollable"
+                  tabindex="-1"
+                  role="dialog"
+                  aria-labelledby="exampleModalScrollableTitle"
+                  aria-hidden="true"
+                >
+                  <div
+                    class="modal-dialog modal-dialog-scrollable"
+                    role="document"
+                  >
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5
+                          class="modal-title"
+                          id="exampleModalScrollableTitle"
+                        >
+                          Saltfish&Dumplings
+                        </h5>
+                        <button
+                          type="button"
+                          class="close"
+                          data-dismiss="modal"
+                          aria-label="Close"
+                        >
+                          <span aria-hidden="true">&times;</span>
+                        </button>
                       </div>
-                    </div>
-                  </div>
-                </div>
+                      <div class="modal-body">
+                        <img
+                          src="https://i.ytimg.com/vi/av0PNCWD0EQ/maxresdefault.jpg"
+                          className="card-img-top"
+                          alt="..."
+                          style={{
+                            width: "90%",
+                            margin: "15px",
+                            borderRadius: "15px"
+                          }}
+                        />
+                      </div>
+                      <div class="modal-footer">
+                      <Link to="/Order">
+                        <button type="button" class="btn btn-primary">
+                          Add to cart- $11.35
+                        </button>
+                        </Link>
+                      </div>
               </div>
-              <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://www.uncommoncaribbean.com/wp-content/uploads/2011/07/caribbean-johnny-cake-recipe.jpeg" className="card-img-top" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Johnny Cake</h5>
-                    <p className="card-text">Sweet flat like shaped deep fried cake with a fluffy doughy inside with a delightful taste </p>
-                    
-
-                      <button className="button2 btn"><Link to="/Order">Order now!</Link></button>
-                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://img.sndimg.com/food/image/upload/c_thumb,q_80,w_412,h_232/v1/img/recipes/17/87/75/MCmXydHzREeVpUel7L8w-IMG_5181.JPG" className="card-img-top" alt="..." style={{width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Banana fritters</h5>
-                    <p className="card-text">Banana pancake like batter fried in low oil. Much softer texture than pancakes which makes a delightful sweet.</p>
-                    
-
-                    <button className="button3 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://img2.10bestmedia.com/Images/Photos/300888/p-KALLALOO_55_660x440.jpg" className="card-img-top" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Kallaloo</h5>
-                    <p className="card-text">Boiled and seasoned spinach, chard, and okra soup
-                     that can be made with seafood or sous(Pig meat)in it.</p>
-                    
-
-                      <button className="button4 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://uncommoncarib-wpengine.netdna-ssl.com/wp-content/uploads/2014/05/Goat-Water-Taste-of-the-Caribbean-2048x1367.jpg;" className="card-img-top" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Goat water</h5>
-                    <p className="card-text">A thin brown soup with parts of goat meat including some bones with cloves and thyme.</p>
-                    
-
-                    <button className="button5 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://sincerelymayra.com/wp-content/uploads/2016/10/bacalaito-SM.jpg" className="card-img-top" alt="..." style={{width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Saltfishcakes</h5>
-                    <p className="card-text">Saltfish mixed in with a thick flour batter, bell pepers, and other spices that then gets fried.</p>
-                    
-
-                      <button className="button6 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://i.pinimg.com/originals/17/c3/90/17c3905169ac94bfcc57da88024d412b.jpg" className="card-img-top" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Stuffing</h5>
-                    <p className="card-text">Colored mashed potatos well seasoned with spices a dash of sugar and sometimes raisins are added.</p>
-                    
-
-                    <button className="button7 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/23707_384885498502_3776900_n.jpg?_nc_cat=102&_nc_sid=a61e81&_nc_ohc=bStGj9JKOTcAX9-xw28&_nc_ht=scontent-mia3-2.xx&oh=621ffff0c1f8186c4378d8ffd94bed93&oe=5E963351" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Salmon Balls</h5>
-                    <p className="card-text">Spiced and seasoned salmon (with the salmon bones removed) balled into an oval like shape and fried.</p>
-                    
-
-                    <button className="button8 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-            <div className="col">  
-                <div className="card" style={{ width: "18rem", margin: "20px" }}>
-                  <img src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-0/p160x160/69790680_2720711057963044_1365460838877495296_o.jpg?_nc_cat=105&_nc_sid=8024bb&_nc_ohc=piklmrOFW6sAX8eQr11&_nc_ht=scontent-mia3-2.xx&_nc_tp=6&oh=3279166c1634d8618dac5c910c85391f&oe=5E9961D2" alt="..." style={{height: "36%", width: "90%", margin: "15px",  borderRadius: "15px" }} />
-                  <div className="card-body">
-                    <h5 className="card-title">Sous&Potato salad</h5>
-                    <p className="card-text">Pig part chunks in a soup like sauce spiced heavily with a side of potato salad</p>
-                    
-
-                    <button className="button9 btn"><Link to="/Order">Order now!</Link></button>                        
-              
-                  </div>
-                </div>
-            </div>
-
             </div>
           </div>
-        
-      </React.Fragment>
-    )
-}
+          {/* thi is the end of card1 */}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default InfoCard;
--- src/views/home.js
+++ src/views/home.js
@@ -1,51 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import STX from '../assets/STX.jpg';
-import STT from '../assets/STT.jpg';
-import STJ from '../assets/STJ.jpg'
+import STX from "../assets/STX.jpg";
+import STT from "../assets/STT.jpg";
+import STJ from "../assets/STJ.jpg";
 import InfoCard from "../components/InfoCard";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
-
-import "./Home.css"
-
+import "./Home.css";
 
 const Home = () => {
+  const [foods, setfoods] = useState([
+    {   "name": "Saltfish&Dumplings",
+        "price": "$13.75"},
+    { "name": "Johnny Cake" },
+    { "name": "Banana Fritters" },
+    { 'name': "Kallaloo" },
+    { 'name': "Goat Soap" },
+    { 'name': "Saltfishcakes" },
+    { 'name': "Stuffing" },
+    { 'name': "Salmon Balls" },
+    { 'name': "Sous&Potato Salad" }
+  ]);
+
+
   return (
-      <React.Fragment>
-        <Navbar />
-        <div id="carouselExampleControls" className="Pop carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={STX} className="d-block w-100" alt="..."/>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h1>St. Croix</h1>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={STT} className="d-block w-100" alt="..."/>
-                </div>
-                <div className="carousel-item">
-                    <img src={STJ} className="d-block w-100" alt="..."/>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h1>St. John</h1>
-                    </div>
-                </div>
+    <React.Fragment>
+      <Navbar />
+      <div
+        id="carouselExampleControls"
+        className="Pop carousel slide"
+        data-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={STX} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h1>St. Croix</h1>
             </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
+          </div>
+          <div className="carousel-item">
+            <img src={STT} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={STJ} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h1>St. John</h1>
             </div>
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleControls"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      <div className="container">
+      <div className="box">
+            {foods.map((food,index) => {
+                return (
+                    <InfoCard key={index} name={food.name} price={food.price}/>
+                )
+            })}
+      </div>
+      </div>
 
-       <InfoCard />
-       <Footer />
-
-      </React.Fragment>
+      <Footer />
+    </React.Fragment>
   );
 };
 
